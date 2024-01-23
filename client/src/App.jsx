218377ac1cf78a39312ae4b85b8aeaa2549df430
Rahmah Putri Azzahra <rahmaputriazzahra@gmail.com>
2024-01-23 14:58:07 +0700

    chore: setup socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+import {io} from 'socket.io-client'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+const socket = io("http://localhost:3000")
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const { createServer } = require('node:http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
