b25a639ee2ce441adea3394f2d28ec68d2e69511
“maulanaFandi” <maulana27fandi@gmail.com>
2024-01-24 14:50:56 +0700

    fix form login & register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,5 @@
-import { RouterProvider, createBrowserRouter, redirect } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import router from './router'
-// import './App.css'
-// import LayOut from './layout/layout'
-// import HomePage from './views/homepage'
-// import Sender from './views/sender'
-// import Reciever from './views/reciever'
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/register",
-//     element: <></>
-//   },
-//   {
-//     path: "/login",
-//   },
-//   {
-//     // loader: ()=> !localStorage.getItem("access_token") && redirect("/login"),
-//     children: [
-//       {
-//         path: "/",
-//         element: <LayOut/>,
-//         children:[
-//           {
-//         path: "/",
-//         element: <HomePage/>,
-//           }
-//         ]
-//       },
-//       {
-//         path: "/sender",
-//         element: <LayOut/>,
-//         children:[
-//           {
-//         path: "/sender",
-//         element: <Sender/>,
-//           }
-//         ]
-//       },
-//       {
-//         path: "/reciever",
-//         element: <LayOut/>,
-//         children:[
-//           {
-//         path: "/reciever",
-//         element: <Reciever/>,
-//           }
-//         ]
-//       },
-//     ]
-//   }
-// ])
 
 function App() {
 
