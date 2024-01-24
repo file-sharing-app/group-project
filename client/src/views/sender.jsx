289191b1e1cfc3604b3bdaa811b3ce3ae3b08c2b
muhammadsubhan14 <muhammadsubhantarmedi@gmail.com>
2024-01-24 19:40:34 +0700

    nothing change

--- client/src/views/sender.jsx
+++ client/src/views/sender.jsx
@@ -23,42 +23,43 @@ function Sender() {
               <h3 className="text-white">Your Code is Here:</h3>
               <h4 className="text-white">000-000-000</h4>
             </div>
-            
+
             <form className="w-full max-w-md lg:col-span-5 lg:pt-2 mt-32">
-                <div className="flex gap-x-4">
-                  <div className="col-span-full">
-                    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white-900/25 px-6 py-10">
-                      <div className="text-center">
-                        <div className="mt-4 flex text-sm leading-6 text-white-800">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                            <span>Upload a file</span>
-                            <input
-                              id="file-upload"
-                              name="file-upload"
-                              type="file"
-                              className="sr-only"
-                            />
-                          </label>
-                          <p className="text-xs leading-5 text-white-800">
-                            or drag and drop
-                          </p>
-                        </div>
+              <div className="flex gap-x-4">
+                <div className="col-span-full">
+                  <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white-900/25 px-6 py-10">
+                    <div className="text-center">
+                      <div className="mt-4 flex text-sm leading-6 text-white-800">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                          />
+                        </label>
                         <p className="text-xs leading-5 text-white-800">
-                          PNG, JPG, GIF up to 10MB
+                          or drag and drop
                         </p>
                       </div>
+                      <p className="text-xs leading-5 text-white-800">
+                        PNG, JPG, GIF up to 10MB
+                      </p>
                     </div>
                   </div>
-                  <button
-                    type="submit"
-                    className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-white-800 shadow-sm hover:bg-sky-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white h-fit self-end">
-                    Send File
-                  </button>
                 </div>
-              </form>
-
+                <button
+                  type="submit"
+                  className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-white-800 shadow-sm hover:bg-sky-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white h-fit self-end"
+                >
+                  Send File
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
